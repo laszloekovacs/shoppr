c55f6f5995dd8e39546eea02020f40876bc31e9b
Laszlo E Kovacs <laszloekovacs@gmail.com>
2023-11-25 18:01:35 +0100

    added more

--- app/features/paypal.ts
+++ app/features/paypal.ts
@@ -1,5 +1,4 @@
 import 'dotenv/config'
-import axios from 'axios'
 import chalk from 'chalk'
 import createLogger from './logger'
 import assert from 'assert'
@@ -8,6 +7,9 @@ const logOk = createLogger('paypal', chalk.cyan, chalk.bgCyan)
 const logError = createLogger('paypal', chalk.red, chalk.bgRed)
 
 const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET, PAYPAL_ENDPOINT } = process.env
+assert(PAYPAL_CLIENT_SECRET, 'Missing PAYPAL_CLIENT_SECRET')
+assert(PAYPAL_CLIENT_ID, 'Missing PAYPAL_CLIENT_ID')
+assert(PAYPAL_ENDPOINT, 'Missing PAYPAL_ENDPOINT')
 
 /**
  * Generate an OAuth 2.0 access token for authenticating with PayPal REST APIs.
@@ -15,28 +17,85 @@ const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET, PAYPAL_ENDPOINT } = process.env
  */
 export const getAccessToken = async () => {
   try {
-    assert(PAYPAL_CLIENT_SECRET, 'Missing PAYPAL_CLIENT_SECRET')
-    assert(PAYPAL_CLIENT_ID, 'Missing PAYPAL_CLIENT_ID')
-    assert(PAYPAL_ENDPOINT, 'Missing PAYPAL_ENDPOINT')
-
     const credentials = Buffer.from(
       `${PAYPAL_CLIENT_ID}:${PAYPAL_CLIENT_SECRET}`,
       'utf-8'
     ).toString('base64')
 
-    const res = await axios.post(
-      `${PAYPAL_ENDPOINT}/v1/oauth2/token`,
-      'grant_type=client_credentials',
-      {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          Authorization: `Basic ${credentials}`,
-        },
-      }
-    )
+    const res = await fetch(`${PAYPAL_ENDPOINT}/v1/oauth2/token`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${credentials}`,
+      },
+      body: 'grant_type=client_credentials',
+    })
 
-    logError(res.data.access_token)
-    return res.data.access_token
+    const data = await res.json()
+
+    return data.access_token
+  } catch (error) {
+    logError(error)
+  }
+}
+
+/**
+ * Create an order to start the transaction.
+ * @see https://developer.paypal.com/docs/api/orders/v2/#orders_create
+ */
+const createOrder = async (cart: string) => {
+  try {
+    logOk('Creating order ' + JSON.stringify(cart))
+
+    const token = await getAccessToken()
+    const url = `${PAYPAL_ENDPOINT}/v2/checkout/orders`
+    const payload = {
+      intent: 'CAPTURE',
+      purchase_units: [
+        {
+          amount: {
+            currency_code: 'HUF',
+            value: '500.00',
+          },
+        },
+      ],
+    }
+
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(payload),
+    })
+
+    const data = await res.json()
+    return data
+  } catch (error) {
+    logError(error)
+  }
+}
+
+/**
+ * Capture payment for the created order to complete the transaction.
+ * @see https://developer.paypal.com/docs/api/orders/v2/#orders_capture
+ */
+const captureOrder = async (cart: string) => {
+  try {
+    const token = await getAccessToken()
+    const url = `${PAYPAL_ENDPOINT}/v2/checkout/orders/${cart}/capture`
+
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    const data = await res.json()
+    return data
   } catch (error) {
     logError(error)
   }
--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react'
+import 'dotenv/config'
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
@@ -20,6 +21,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script src="https://www.paypal.com/sdk/js?client-id=ASi0nLR3W3Z5QQ-37Prv41WJ_EywNp5ttaLY2RUdVuhc_d7D17tIBZuWk3OmBhFbG31NEz2McKhVQTB9&currency=HUF"></script>
         <Meta />
         <Links />
       </head>
