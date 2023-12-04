e27b463303699dfc1fdb491892eaf6f9b714b1a4
laszloekovacs <laszloekovacs@gmail.com>
2023-12-04 22:51:39 +0100

    renamed index

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from '@remix-run/node'
 import Header from '../components/header'
+import { Outlet } from '@remix-run/react'
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,6 +13,7 @@ export default function Index() {
   return (
     <section id="index">
       <Header />
+      <Outlet />
     </section>
   )
 }
