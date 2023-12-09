233fabe2da5b34c40f0276bcae8130115c046a7a
Laszlo E Kovacs <laszloekovacs@gmail.com>
2023-12-09 14:25:24 +0100

    formatted

--- _test_/dashboard.category.create.test.tsx
+++ _test_/dashboard.category.create.test.tsx
@@ -2,29 +2,29 @@ import '@testing-library/jest-dom'
 import { render, screen, waitFor } from '@testing-library/react'
 import { createRemixStub } from '@remix-run/testing'
 import CreateCategoryPage, {
-    loader,
-    action,
+	loader,
+	action,
 } from '@/routes/dashboard.category.create'
 import App from '@/root'
 
 describe('dashboard.category.create', () => {
-    const Stub = createRemixStub([
-        {
-            path: '/',
-            Component: CreateCategoryPage,
-            loader,
-            action,
-        },
-    ])
+	const Stub = createRemixStub([
+		{
+			path: '/',
+			Component: CreateCategoryPage,
+			loader,
+			action,
+		},
+	])
 
-    it('renders without crashing', async () => {
-        render(<Stub />)
-    })
+	it('renders without crashing', async () => {
+		render(<Stub />)
+	})
 
-    it('has a form with a submit input', async () => {
-        render(<Stub />)
-        await waitFor(() => {
-            screen.findByRole('submit')
-        })
-    })
+	it('has a form with a submit input', async () => {
+		render(<Stub />)
+		await waitFor(() => {
+			screen.findByRole('submit')
+		})
+	})
 })
--- _test_/mongoose.test.ts
+++ _test_/mongoose.test.ts
@@ -5,8 +5,8 @@ import mongoose from 'mongoose'
 import { connectDatabase } from '../app/services/mongoose.server'
 
 describe('mongoose', () => {
-    it('able to connect to database', async () => {
-        await connectDatabase()
-        expect(mongoose.connection.readyState).toBeGreaterThan(1)
-    })
+	it('able to connect to database', async () => {
+		await connectDatabase()
+		expect(mongoose.connection.readyState).toBeGreaterThan(1)
+	})
 })
