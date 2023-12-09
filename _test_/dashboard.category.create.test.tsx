6cb9755f2761a4e313f164f37649b867145291f3
Laszlo E Kovacs <laszloekovacs@gmail.com>
2023-12-09 14:13:45 +0100

    tested dashboard

--- _test_/dashboard.category.create.test.tsx
+++ _test_/dashboard.category.create.test.tsx
@@ -1,22 +1,30 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import { createRemixStub } from '@remix-run/testing'
 import CreateCategoryPage, {
     loader,
     action,
 } from '@/routes/dashboard.category.create'
+import App from '@/root'
 
 describe('dashboard.category.create', () => {
-    it('renders without crashing', () => {
-        const Stub = createRemixStub([
-            {
-                path: '/',
-                Component: CreateCategoryPage,
-                loader,
-                action,
-            },
-        ])
+    const Stub = createRemixStub([
+        {
+            path: '/',
+            Component: CreateCategoryPage,
+            loader,
+            action,
+        },
+    ])
 
+    it('renders without crashing', async () => {
         render(<Stub />)
+    })
+
+    it('has a form with a submit input', async () => {
+        render(<Stub />)
+        await waitFor(() => {
+            screen.findByRole('submit')
+        })
     })
 })
