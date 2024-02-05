5d57e2bf93d442fac9f49d100ef2f32b84c0ea59
Laszlo E Kovacs <laszloekovacs@gmail.com>
2024-02-05 14:27:16 +0100

    added card

---
+++ app/components/Card.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled'
+import React from 'react'
+
+const CardUnstyled = () => {
+	return <div>Card</div>
+}
+
+const Card = styled(CardUnstyled)`
+	background: #f2f2f2;
+`
+
+export default Card
---
+++ stories/Card.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import Card from '../app/components/Card'
+
+const meta = {
+	title: 'Example/Card',
+	component: Card,
+} satisfies Meta<typeof Card>
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+	render: () => <Card />,
+}
