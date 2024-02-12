14d2dcaef4fdb094a6d72e60994c2d3b77baeece
laszloekovacs <laszloekovacs@gmail.com>
2024-02-12 12:54:12 +0100

    added product type guards

--- app/models/product.ts
+++ app/models/product.ts
@@ -6,24 +6,26 @@ export interface ProductSchema {
 	brand: string
 	department: string
 	isPublished: boolean
-	attribute: Array<ProductAttribute>
+	images: Array<string>
+	attributes: Array<ProductAttribute>
 	options: Array<ProductOption>
 }
 
-export function isProductSchema(product: any): product is ProductSchema {
+export function isProductSchema(obj: any): obj is ProductSchema {
 	if (
-		'name' in product &&
-		'brand' in product &&
-		'department' in product &&
-		'isPublished' in product &&
-		'attribute' in product &&
-		'options' in product &&
-		Array.isArray(product['attribute']) &&
-		Array.isArray(product['options'])
+		typeof obj.name == 'string' &&
+		typeof obj.brand == 'string' &&
+		typeof obj.department == 'string' &&
+		typeof obj.isPublished == 'boolean' &&
+		Array.isArray(obj.images) &&
+		obj.images.every((i: any) => typeof i == 'string') &&
+		Array.isArray(obj.attributes) &&
+		obj.attributes.every(isAttribute) &&
+		Array.isArray(obj.options) &&
+		obj.options.every(isOption)
 	) {
 		return true
 	}
-
 	return false
 }
 
@@ -35,6 +37,18 @@ export interface ProductAttribute {
 	unit?: string
 }
 
+export function isAttribute(obj: any): obj is ProductAttribute {
+	if (
+		typeof obj.name == 'string' &&
+		typeof obj.displayName == 'string' &&
+		['string', 'number', 'boolean'].includes(typeof obj.value) &&
+		('unit' in obj ? typeof obj.unit == 'string' : true)
+	) {
+		return true
+	}
+	return false
+}
+
 /* various options for a product like XL shirt, M shirt */
 export interface ProductOption {
 	name: string
@@ -43,4 +57,18 @@ export interface ProductOption {
 	isDiscounted: boolean
 	sku: string
 	stock: number
+}
+
+export function isOption(obj: any): obj is ProductOption {
+	if (
+		typeof obj.name == 'string' &&
+		typeof obj.price == 'number' &&
+		('discount' in obj ? typeof obj.discount == 'number' : true) &&
+		typeof obj.isDiscounted == 'boolean' &&
+		typeof obj.sku == 'string' &&
+		typeof obj.stock == 'number'
+	) {
+		return true
+	}
+	return false
 }
