1499836b803eed6fdb3089cb06fd52fff5b6424e
laszloekovacs <laszloekovacs@gmail.com>
2024-02-13 13:02:45 +0100

    added user data display at login

--- app/routes/dashboard.login.tsx
+++ app/routes/dashboard.login.tsx
@@ -1,9 +1,21 @@
-import { Form } from '@remix-run/react'
+import { LoaderFunctionArgs, json } from '@remix-run/node'
+import { Form, useLoaderData } from '@remix-run/react'
 import React from 'react'
+import { authenticator } from '~/services/session.server'
+
+export async function loader({ request }: LoaderFunctionArgs) {
+	const user = await authenticator.isAuthenticated(request)
+
+	return json({ user })
+}
 
 const LoginPage = () => {
+	const { user } = useLoaderData<typeof loader>()
+
 	return (
 		<div>
+			<pre>{JSON.stringify(user, null, 2)}</pre>
+
 			<Form action="/api/auth0" method="post">
 				<button>Login with Auth0</button>
 			</Form>
