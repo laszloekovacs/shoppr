60ff6b257e7e3a7a4c2a8cca4f4f2bb8899fa9fb
Laszlo E Kovacs <laszloekovacs@gmail.com>
2024-02-16 15:05:52 +0100

    added paypal client id

--- app/routes/dashboard.payout.tsx
+++ app/routes/dashboard.payout.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
 import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js'
+import { json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+
+export const loader = () => {
+	const clientId = process.env.PAYPAL_CLIENT_ID as string
+
+	return json({ clientId })
+}
 
 const PayoutPage = () => {
+	const { clientId } = useLoaderData<typeof loader>()
+
 	return (
 		<PayPalScriptProvider
-			options={{ clientId: 'test', currency: 'USD', intent: 'capture' }}
+			options={{ clientId: clientId, currency: 'HUF', intent: 'capture' }}
 		>
 			<PayPalButtons style={{ layout: 'vertical' }} />
 		</PayPalScriptProvider>
