88425343d90983bea8b61a4c9670c4c8a8ef515b
Laszlo E Kovacs <laszloekovacs@gmail.com>
2024-02-17 21:03:13 +0100

    added more tests

--- __tests/mongodb.test.ts
+++ __tests/mongodb.test.ts
@@ -72,7 +72,7 @@ describe('mongodb queries', () => {
 	})
 
 	//
-	it('tries to use $inc operation on a boolean, it rejects', async () => {
+	it('tries to use $inc operation on a boolean, it rejects $inc', async () => {
 		const data = {
 			url: 'test',
 			switcheroo: true,
@@ -89,7 +89,7 @@ describe('mongodb queries', () => {
 		expect(result).rejects.toThrow(/\$inc/)
 	})
 
-	it('increases a keys value', async () => {
+	it('increases a keys value - $inc', async () => {
 		const data = {
 			url: 'test',
 			count: 0,
@@ -116,7 +116,7 @@ describe('mongodb queries', () => {
 		expect(result.modifiedCount).toBe(1)
 	})
 
-	it('is able to unset unwanted fields', async () => {
+	it('is able to unset unwanted fields - $unset', async () => {
 		const data = {
 			name: 'test',
 			favorites: [
@@ -148,7 +148,7 @@ describe('mongodb queries', () => {
 		expect(result.acknowledged).toBe(true)
 	})
 
-	it('pushes into an array', async () => {
+	it('pushes into an array - $push', async () => {
 		const data = {
 			name: 'star wars episode 1',
 		}
@@ -172,7 +172,7 @@ describe('mongodb queries', () => {
 		expect(result?.comments).toHaveLength(1)
 	})
 
-	it('can push more than one item into an array', async () => {
+	it('can push more than one item into an array - $push', async () => {
 		const data = {
 			name: 'steeve',
 			scores: [],
@@ -199,4 +199,149 @@ describe('mongodb queries', () => {
 		expect(result?.scores).toContain(3)
 		expect(result?.scores).toEqual([2, 3, 4])
 	})
+
+	it('adds to a set - $addToSet, $ne, $each', async () => {
+		const data = {
+			name: 'steeve',
+			favorites: {
+				books: ['one', 'two'],
+			},
+		}
+
+		await collection.insertOne(data)
+
+		await collection.updateOne(
+			{ 'favorites.books': { $ne: 'three' } },
+			{
+				$addToSet: { 'favorites.books': { $each: ['three', 'five'] } },
+			}
+		)
+
+		await collection.updateOne(
+			{
+				'favorites.books': { $ne: 'four' },
+			},
+			{
+				$push: {
+					'favorites.books': 'four',
+				},
+			}
+		)
+
+		const result = await collection.findOne({ name: 'steeve' })
+
+		expect(result?.favorites.books).toContain('three')
+		expect(result?.favorites.books).toContain('four')
+	})
+
+	it('pops out one element - $pop', async () => {
+		const data = {
+			tasks: ['dishes', 'laundry', 'cooking'],
+		}
+
+		await collection.insertOne(data)
+
+		await collection.updateOne(
+			{},
+			{
+				$pull: { tasks: 'dishes' },
+			}
+		)
+
+		// pull last element
+		await collection.updateOne(
+			{},
+			{
+				$pop: { tasks: -1 },
+			}
+		)
+
+		const result = await collection.findOne<typeof data>({})
+
+		expect(result?.tasks).not.toContain('dishes')
+		expect(result?.tasks).toHaveLength(1)
+	})
+
+	it('can use indexing into arrays, uses array match $ op', async () => {
+		const data = {
+			content: 'the content',
+			comments: [
+				{
+					comment: 'good post',
+					author: 'John',
+					votes: 0,
+				},
+				{
+					comment: 'i thought it was too short',
+					author: 'Claire',
+					votes: 3,
+				},
+				{
+					comment: 'free watches',
+					author: 'Alice',
+					votes: -5,
+				},
+				{
+					comment: 'vacation getaways',
+					author: 'Lynn',
+					votes: -7,
+				},
+			],
+		}
+
+		await collection.insertOne(data)
+
+		// increment blog post vote by index
+		await collection.updateOne(
+			{ content: 'the content' },
+			{
+				$inc: { 'comments.0.votes': 1 },
+			}
+		)
+
+		// increment by finding the author
+		await collection.updateOne(
+			{ 'comments.author': 'Lynn' },
+			{ $set: { 'comments.$.votes': 10 } }
+		)
+
+		const result = await collection.findOne<typeof data>({})
+
+		expect(result?.comments[3].votes).toBe(10)
+		expect(result?.comments[0].votes).toBe(1)
+	})
+
+	it('uses array filter - arrayFilter', async () => {
+		const data = {
+			posts: [
+				{
+					text: 'ok',
+					votes: 6,
+				},
+				{
+					text: 'hide',
+					votes: -7,
+				},
+			],
+		}
+
+		await collection.insertOne(data)
+
+		await collection.updateOne(
+			{},
+			{
+				$set: { 'posts.$[elem].hidden': true },
+			},
+			{
+				arrayFilters: [{ 'elem.votes': { $lte: -5 } }],
+			}
+		)
+
+		const result = await collection.findOne({})
+
+		expect(result?.posts[1]?.hidden).toBe(true)
+		//
+	})
+
+	// end
 })
