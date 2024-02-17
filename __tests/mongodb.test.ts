b8115587b6df3cf7818be4ed50ae11de8c7d6c71
Laszlo E Kovacs <laszloekovacs@gmail.com>
2024-02-17 17:39:06 +0100

    configured vitest

---
+++ __tests/mongodb.test.ts
@@ -0,0 +1,5 @@
+describe('mongodb', () => {
+	it('works', () => {
+		expect(true).toBe(true)
+	})
+})
--- remix.env.d.ts
+++ remix.env.d.ts
@@ -1,2 +1,5 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/node" />
+/// <reference types="@remix-run/react" />
+/// <reference types="@remix-run/serve" />
+/// <reference types="vitest/globals" />
