833ba01a131edef555584f6884b4463fbce88a19
laszloekovacs <laszloekovacs@gmail.com>
2024-02-20 16:35:47 +0100

    eod

--- app/routes/checkout.tsx
+++ app/routes/checkout.tsx
@@ -5,9 +5,8 @@ import {
 	OnApproveData,
 } from '@paypal/paypal-js'
 import { PayPalButtons, PayPalScriptProvider } from '@paypal/react-paypal-js'
-import { LoaderFunctionArgs, json } from '@remix-run/node'
-import { useFetcher, useLoaderData } from '@remix-run/react'
-import RawData from '~/components/raw-data'
+import { ActionFunctionArgs, LoaderFunctionArgs, json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
 import { documents } from '~/services/db.server'
 import { authenticator } from '~/services/session.server'
 
@@ -17,19 +16,59 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 
 	const clientId = process.env.PAYPAL_CLIENT_ID as string
 
+	console.log(clientId)
+
 	return json({ account, clientId })
 }
 
 export default function Checkout() {
-	const { account } = useLoaderData<typeof loader>()
+	const { account, clientId } = useLoaderData<typeof loader>()
 
 	return (
 		<div>
 			<h1>Checkout</h1>
 
 			<div>
-				<PayPalScriptProvider options={{ clientId: 'test' }}>
-					<PayPalButtons createOrder={createOrder} onApprove={onApprove} />
+				<PayPalScriptProvider
+					options={{ clientId: 'test', currency: 'HUF', intent: 'capture' }}
+				>
+					<PayPalButtons
+						createOrder={async data => {
+							return fetch('/checkout', {
+								method: 'POST',
+								headers: {
+									'Content-Type': 'application/json',
+								},
+								body: JSON.stringify({
+									intent: 'CREATE',
+									cart: [
+										{
+											this: 'item',
+											amount: 'zero',
+										},
+									],
+								}),
+							})
+								.then(response => response.json())
+								.then(order => order.id)
+						}}
+						onApprove={async data => {
+							return fetch('/checkout', {
+								method: 'POST',
+								headers: {
+									'Content-Type': 'application/json',
+								},
+								body: JSON.stringify({
+									intent: 'APPROVE',
+									orderID: data.orderID,
+								}),
+							})
+								.then(response => response.json())
+								.then(orderData => {
+									alert('ok')
+								})
+						}}
+					/>
 				</PayPalScriptProvider>
 			</div>
 
@@ -38,16 +77,16 @@ export default function Checkout() {
 	)
 }
 
-const createOrder = async (
-	data: CreateOrderData,
-	actions: CreateOrderActions
-): Promise<string> => {
-	return ''
-}
+export const action = async ({ request }: ActionFunctionArgs) => {
+	const body = await request.json()
+	console.log(body)
 
-const onApprove = async (
-	data: OnApproveData,
-	actions: OnApproveActions
-): Promise<void> => {
-	return
+	switch (body.intent) {
+		case 'CREATE': {
+			return json({ status: 'created' })
+		}
+		case 'APPROVE': {
+			return json({ status: 'approved' })
+		}
+	}
 }
