5dd74adaf2641dfe7f68db4b14ce6c9775e35381
Laszlo E Kovacs <laszloekovacs@gmail.com>
2024-02-21 21:47:48 +0100

    added test text to stylesheet route

--- app/components/bigtext.tsx
+++ app/components/bigtext.tsx
@@ -1,3 +1,3 @@
 export const BigText = () => (
-	<h1 className="text-3xl font-bold underline">Hello world!</h1>
+	<h1 className="text-4xl font-bold">Hello world!</h1>
 )
--- app/routes/stylesheet.tsx
+++ app/routes/stylesheet.tsx
@@ -1,5 +1,11 @@
+import { BigText } from '~/components/bigtext'
+
 const StyleBook = () => {
-	return <div></div>
+	return (
+		<div>
+			<BigText></BigText>
+		</div>
+	)
 }
 
 export default StyleBook
